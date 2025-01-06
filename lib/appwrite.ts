dbc2fc2c04dbca0d16ec15de30bd23bd347017ac
Ebrahim Beiaty <ebrahimbeiaty@gmail.com>
2025-01-06 22:34:16 +0000

    database connection

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,24 +1,32 @@
-import { Client,Avatars, Account, OAuthProvider } from "react-native-appwrite";
+import { Client,Avatars, Account, OAuthProvider, Databases } from "react-native-appwrite";
 import * as Linking from "expo-linking";
 import { openAuthSessionAsync } from "expo-web-browser";
 
 
 export const config = {
     platform: "com.homefinder",
-    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT ,
-    project: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+  galleriesCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
+  reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
+  agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENTS_COLLECTION_ID,
+  propertiesCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
 }
 
 export const client = new Client();
 client
     .setEndpoint(config.endpoint!)
-    .setProject(config.project!)
+    .setProject(config.projectId!)
     .setPlatform(config.platform!)
 
 
 
     export const avatar = new Avatars(client);
     export const account = new Account(client);
+    export const database = new Databases(client);
 
 
     export async function login() {
